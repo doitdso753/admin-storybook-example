9e417cb0844951d8d3aed8cd7050024e1b64a9bf
ParkSoHyeon <dso753@naver.com>
2022-06-13 01:47:28 +0900

    GlobalStyle storybook에 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,15 @@
+import React from "react";
+import {GlobalStyle} from '../src/components/common'
+
+export const decorators = [
+  (Story) => (
+      <>
+        <GlobalStyle/>
+        <Story />
+      </>
+  )
+]
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
